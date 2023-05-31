20bf04dae99293d5b1e85296c3a6303a2b4217db
jhdwcry <korn.anastasia02@gmail.com>
2023-05-31 19:08:28 +0300

    test commit

--- dev/assets/js/custom.js
+++ dev/assets/js/custom.js
@@ -1,5 +1,16 @@
-$( document ).ready(function() {
-    $('.first-screen-slider').slick();
+$(document).ready(function() {
+    $('.first-screen-slider').slick({
+        dots: true,
+        speed: 1000,
+        appendArrows: ".container-arrows",
+
+        responsive: [{
+            breakpoint: 991,
+            settings: {
+                arrows: false
+            }
+        }]
+    });
 
 
 });
