eddb8ba3c034bb8fdb6b5a0141c4548a95d3912d
jhdwcry <korn.anastasia02@gmail.com>
2023-06-11 13:36:03 +0300

    test commit

--- dev/assets/js/custom.js
+++ dev/assets/js/custom.js
@@ -27,3 +27,30 @@ if (document.querySelector('.calculation-form')) {
 } else {
     null
 }
+
+
+mobileOnlySlider(".advantages-slider", true, false, 991);
+
+function mobileOnlySlider($slidername, $dots, $arrows, $breakpoint) {
+    var slider = $($slidername);
+    var settings = {
+        mobileFirst: true,
+        dots: $dots,
+        arrows: $arrows,
+        responsive: [{
+            breakpoint: $breakpoint,
+            settings: "unslick"
+        }]
+    };
+
+    slider.slick(settings);
+
+    $(window).on("resize", function() {
+        if ($(window).width() > $breakpoint) {
+            return;
+        }
+        if (!slider.hasClass("slick-initialized")) {
+            return slider.slick(settings);
+        }
+    });
+} 
